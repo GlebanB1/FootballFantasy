24592ccda18d6d7c5783398bcfe4495333b1a9e3
GlebanB1 <whozer111@outlook.com>
2023-05-17 20:51:31 +0300

    Version 0.004 by GlebB1

--- app/src/main/java/com/example/footballfantasy/MainActivity.kt
+++ app/src/main/java/com/example/footballfantasy/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.footballfantasy
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.os.Handler
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,16 +15,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
         supportActionBar?.hide()
+
+        var progressBar = findViewById(R.id.progressBar) as ProgressBar
+        progressBar.max=1000
+
+        val currentProgress = 1000
+
+        ObjectAnimator.ofInt(progressBar,"progress",currentProgress)
+            .setDuration(3000)
+            .start()
 
         Handler().postDelayed({
             val intent = Intent(this@MainActivity, ProfileActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000)
+
+
+
 
     }
 }
