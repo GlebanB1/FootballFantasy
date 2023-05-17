e835007e8afc79a7422f343c767646b91d29a7bc
rilyasov <ilyasov.2003@gmail.com>
2023-05-17 18:55:12 +0300

    Riyad 1 third activity

--- app/src/main/java/com/example/footballfantasy/ProfileActivity.kt
+++ app/src/main/java/com/example/footballfantasy/ProfileActivity.kt
@@ -2,10 +2,24 @@ package com.example.footballfantasy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        supportActionBar?.hide()
+
+        val button = findViewById(R.id.button) as Button
+
+        button.setOnClickListener {
+            val intent = Intent(this@ProfileActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
