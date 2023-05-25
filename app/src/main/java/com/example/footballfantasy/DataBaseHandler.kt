eeca5238a9090394918a67c1ff0918b23c7c0823
rilyasov <ilyasov.2003@gmail.com>
2023-05-25 16:05:55 +0300

    login version 1.0

--- app/src/main/java/com/example/footballfantasy/DataBaseHandler.kt
+++ app/src/main/java/com/example/footballfantasy/DataBaseHandler.kt
@@ -2,6 +2,7 @@ package com.example.footballfantasy
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.widget.Toast
@@ -14,7 +15,6 @@ val COL_ID = "id"
 class DataBaseHandler(private val context: Context) : SQLiteOpenHelper(context, DB_NAME, null, 1) {
 
     override fun onCreate(db: SQLiteDatabase?) {
-
         val createTable = "CREATE TABLE $TABLE_NAME (" +
                 "$COL_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 "$COL_LOGIN VARCHAR(256))"
@@ -29,9 +29,14 @@ class DataBaseHandler(private val context: Context) : SQLiteOpenHelper(context, 
     }
 
     fun insertData(login: User): Long {
+        if (isLoginExists(login.login)) {
+            Toast.makeText(context, "Login already exists", Toast.LENGTH_SHORT).show()
+            return -1L
+        }
+
         val db = this.writableDatabase
         val contentValues = ContentValues()
-        contentValues.put(COL_LOGIN, login.login) // Pass login.login instead of login
+        contentValues.put(COL_LOGIN, login.login)
         val result = db.insert(TABLE_NAME, null, contentValues)
         if (result == -1L)
             Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
@@ -40,4 +45,13 @@ class DataBaseHandler(private val context: Context) : SQLiteOpenHelper(context, 
         return result
     }
 
+    // Change visibility to public
+    fun isLoginExists(login: String): Boolean {
+        val db = this.readableDatabase
+        val query = "SELECT $COL_LOGIN FROM $TABLE_NAME WHERE $COL_LOGIN = ?"
+        val cursor: Cursor? = db.rawQuery(query, arrayOf(login))
+        val exists = cursor?.count ?: 0 > 0
+        cursor?.close()
+        return exists
+    }
 }
--- app/src/main/java/com/example/footballfantasy/LoginActivity.kt
+++ app/src/main/java/com/example/footballfantasy/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.footballfantasy
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -23,12 +24,25 @@ class LoginActivity : AppCompatActivity() {
         btnInsert.setOnClickListener {
             val login = etvLogin.text.toString().trim()
             if (login.isNotEmpty()) {
-                var user = User(etvLogin.text.toString())
                 val db = DataBaseHandler(this)
-                db.insertData(user)
+                if (db.isLoginExists(login)) {
+                    // Login exists, redirect to StartGameActivity
+                    Toast.makeText(this, "Login already exists", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this@LoginActivity, StartGame::class.java)
+                    startActivity(intent)
+                } else {
+                    // Login doesn't exist, insert into table and redirect to CreateClubActivity
+                    val user = User(login)
+                    val insertResult = db.insertData(user)
+                    if (insertResult != -1L) {
+                        val intent = Intent(this@LoginActivity, ClubCreation::class.java)
+                        startActivity(intent)
+                    }
+                }
             } else {
                 Toast.makeText(this, "Please enter a login", Toast.LENGTH_SHORT).show()
             }
         }
+
     }
 }
